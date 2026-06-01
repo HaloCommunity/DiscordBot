6be2cc9e344e08729f63aa52591f5bbdcfbadce6
Mikey O'Toole <mikey@homotechsual.dev>
2026-06-01 20:56:49 +0100

    feat: update version to 1.1.52 and fix EF migration discovery for Halo status incident thread mapping

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to HaloCommunityBot will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.52] - 2026-06-01
+
+### Changed
+
+* Fix EF migration discovery for Halo status incident thread mapping
+
 ## [1.1.51] - 2026-06-01
 
 ### Changed
--- src/HaloCommunityBot/Migrations/20260601120000_AddHaloStatusIncidentMessageMap.cs
+++ src/HaloCommunityBot/Migrations/20260601120000_AddHaloStatusIncidentMessageMap.cs
@@ -1,10 +1,14 @@
+using DiscordBot.Core.Data;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 #nullable disable
 
 namespace HaloCommunityBot.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(HaloCommunityBotContext))]
+    [Migration("20260601120000_AddHaloStatusIncidentMessageMap")]
     public partial class AddHaloStatusIncidentMessageMap : Migration
     {
         /// <inheritdoc />
