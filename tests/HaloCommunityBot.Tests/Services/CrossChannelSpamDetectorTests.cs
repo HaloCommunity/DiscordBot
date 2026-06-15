d8194498d4e75405e93a02c0446ef573d52260f6
Mikey O'Toole <mikey@homotechsual.dev>
2026-06-15 15:40:58 +0100

    feat: enhance cross-channel spam detection with live testing and improved fingerprinting; update changelog to version 1.4.5

--- tests/HaloCommunityBot.Tests/Services/CrossChannelSpamDetectorTests.cs
+++ tests/HaloCommunityBot.Tests/Services/CrossChannelSpamDetectorTests.cs
@@ -13,17 +13,25 @@ public class CrossChannelSpamDetectorTests
     }
 
     [Fact]
-    public void ComputeFingerprint_AttachmentsOnly_ReturnsEmptyTextWithSortedFilenames()
+    public void ComputeFingerprint_AttachmentsOnly_ReturnsSignatureBasedFingerprint()
     {
-        var fingerprint = CrossChannelSpamDetector.ComputeFingerprint("", ["zebra.png", "apple.jpg"]);
-        Assert.Equal("|apple.jpg,zebra.png", fingerprint);
+        var fingerprint = CrossChannelSpamDetector.ComputeFingerprint("", [
+            new AttachmentInfo("zebra.png", 200, 640, 480, "image/png", false),
+            new AttachmentInfo("apple.jpg", 100, 320, 200, "image/jpeg", false)
+        ]);
+
+        Assert.Equal("|image/jpeg:100:320:200:.jpg:0,image/png:200:640:480:.png:0", fingerprint);
     }
 
     [Fact]
     public void ComputeFingerprint_TextAndAttachments_ReturnsBothParts()
     {
-        var fingerprint = CrossChannelSpamDetector.ComputeFingerprint("check this", ["b.png", "a.png"]);
-        Assert.Equal("check this|a.png,b.png", fingerprint);
+        var fingerprint = CrossChannelSpamDetector.ComputeFingerprint("check this", [
+            new AttachmentInfo("b.png", 200, 640, 480, "image/png", false),
+            new AttachmentInfo("a.png", 100, 320, 200, "image/png", false)
+        ]);
+
+        Assert.Equal("check this|image/png:100:320:200:.png:0,image/png:200:640:480:.png:0", fingerprint);
     }
 
     [Fact]
@@ -34,10 +42,19 @@ public class CrossChannelSpamDetectorTests
     }
 
     [Fact]
-    public void ComputeFingerprint_AttachmentOrderIsAlwaysSorted()
+    public void ComputeFingerprint_AttachmentSignatureOrderIsAlwaysSorted()
     {
-        var fp1 = CrossChannelSpamDetector.ComputeFingerprint("", ["z.png", "a.png", "m.png"]);
-        var fp2 = CrossChannelSpamDetector.ComputeFingerprint("", ["a.png", "m.png", "z.png"]);
+        var fp1 = CrossChannelSpamDetector.ComputeFingerprint("", [
+            new AttachmentInfo("z.png", 300, 100, 100, "image/png", false),
+            new AttachmentInfo("a.png", 100, 100, 100, "image/png", false),
+            new AttachmentInfo("m.png", 200, 100, 100, "image/png", false)
+        ]);
+        var fp2 = CrossChannelSpamDetector.ComputeFingerprint("", [
+            new AttachmentInfo("a.png", 100, 100, 100, "image/png", false),
+            new AttachmentInfo("m.png", 200, 100, 100, "image/png", false),
+            new AttachmentInfo("z.png", 300, 100, 100, "image/png", false)
+        ]);
+
         Assert.Equal(fp1, fp2);
     }
 }
